d8a6aa7d3a57ca0368525c7feb0473905751dab7
schen149 <schen149@illinois.edu>
2019-06-11 21:05:32 -0400

    WIP: lru cache

--- app/models.py
+++ app/models.py
@@ -5,6 +5,7 @@ class QueryLog(models.Model):
     query_claim = models.TextField()
     query_time = models.DateTimeField()
 
+
 class FeedbackRecord(models.Model):
     claim = models.TextField()
     perspective = models.TextField()
@@ -12,3 +13,7 @@ class FeedbackRecord(models.Model):
     stance_score = models.FloatField()
     evidence = models.TextField(default="")
     feedback = models.BooleanField() # True = Like, False = Dislike
+
+
+class LRUCache(models.Model):
+    claim = models.TextField()
