2e8cefdef0105e1f64f3728219486e83907d38ad
schen149 <schen149@illinois.edu>
2019-08-12 13:55:58 -0400

    fix api path

--- app/urls.py
+++ app/urls.py
@@ -30,7 +30,7 @@ urlpatterns = [
     path('load_new_claim_text/', views.load_new_claim_text),
     path('api/submit_query_log/', views.api_submit_query_log),
     path('api/submit_feedback/', views.api_submit_feedback),
-    path('api/submit_annotation', views.api_submit_annotation),
+    path('api/submit_annotation/', views.api_submit_annotation),
     path('api/retrieve_evidence/', views.api_retrieve_evidence),
     path('api/test_es_connection/', views.api_test_es_connection),
     path(r'^djga/', include('google_analytics.urls')),
