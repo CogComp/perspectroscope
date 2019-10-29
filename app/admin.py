6d25924eeaa7c2938a2c9a5f753a5db8e3a8b0aa
Daniel Khashabi <khashab2@illinois.edu>
2019-10-28 21:20:42 -0700

    Create admin.py

---
+++ app/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from .models import QueryLog, FeedbackRecord, Claim, EvidenceFeedbackRecord, Perspectives, LRUCache
+
+admin.site.register(QueryLog)
+admin.site.register(FeedbackRecord)
+admin.site.register(Claim)
+admin.site.register(EvidenceFeedbackRecord)
+admin.site.register(Perspectives)
+admin.site.register(LRUCache)
